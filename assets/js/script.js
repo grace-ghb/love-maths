74f58e04f0e68a0b90d68a19fd9fd0d4c2cec474
grace-ghb <>
2023-07-09 19:34:24 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestionr() {
+
+}
